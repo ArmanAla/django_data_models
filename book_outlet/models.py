6e8aa5599c8dac4ebf6835c89f078e91ae60fe12
Ala9921160003 <90964584+Ala9921160003@users.noreply.github.com>
2022-07-21 04:41:42 +0430

    save method was updated in order to sset slug property automatically.

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -14,6 +15,11 @@ class Book(models.Model):
     
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.id])
+    
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
     
     
     def __str__(self):
